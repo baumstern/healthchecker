149a07c4b7b3ef385af2fc9e355aa65670637c5d
Daehyun Paik <paik@a30a.dev>
2022-01-25 14:38:35 +0900

    Check if currently watching specific blockchain

--- pkg/api/watch.go
+++ pkg/api/watch.go
@@ -13,17 +13,20 @@ type cancel func()
 type klaytnClient struct {
 	client      klaytn.Client
 	cancelWatch cancel
+	isWatching  bool
 }
 
 type ethereumClient struct {
 	client      ethereum.Client
 	cancelWatch cancel
+	isWatching  bool
 }
 
 type WatchService interface {
 	Start(blockchainType string) error
 	Stop(blockchainType string) error
 	GetLatestBlock(blockchainType string) (*collector.LatestBlock, error)
+	IsWatching(blockchainType string) (bool, error)
 }
 
 type watchService struct {
@@ -33,19 +36,28 @@ type watchService struct {
 
 func NewWatchService(cfg *config.Config) WatchService {
 	return &watchService{
-		ethereum: ethereumClient{*ethereum.NewClient(cfg), nil},
-		klaytn:   klaytnClient{*klaytn.NewClient(cfg), nil},
+		ethereum: ethereumClient{*ethereum.NewClient(cfg), nil, false},
+		klaytn:   klaytnClient{*klaytn.NewClient(cfg), nil, false},
 	}
 }
 
 func (w *watchService) Start(blockchainType string) error {
 	switch blockchainType {
 	case "ethereum":
-		w.ethereum.cancelWatch = w.ethereum.client.Watch()
+		if !w.ethereum.isWatching {
+			w.ethereum.cancelWatch = w.ethereum.client.Watch()
+			w.ethereum.isWatching = true
+		}
 		return nil
+
 	case "klaytn":
-		w.klaytn.cancelWatch = w.klaytn.client.Watch()
+		if !w.klaytn.isWatching {
+			w.klaytn.cancelWatch = w.klaytn.client.Watch()
+			w.klaytn.isWatching = true
+		}
+
 		return nil
+
 	default:
 		return fmt.Errorf("failed to start watching blockchain: %s is not supported", blockchainType)
 	}
@@ -54,10 +66,18 @@ func (w *watchService) Start(blockchainType string) error {
 func (w *watchService) Stop(blockchainType string) error {
 	switch blockchainType {
 	case "ethereum":
-		w.ethereum.cancelWatch()
+		if w.ethereum.isWatching {
+			w.ethereum.cancelWatch()
+			w.ethereum.isWatching = false
+		}
+
 		return nil
 	case "klaytn":
-		w.klaytn.cancelWatch()
+		if w.klaytn.isWatching {
+			w.klaytn.cancelWatch()
+			w.klaytn.isWatching = false
+		}
+
 		return nil
 	default:
 		return fmt.Errorf("failed to stop watching blockchain: %s is not supported", blockchainType)
@@ -72,5 +92,16 @@ func (w *watchService) GetLatestBlock(blockchainType string) (*collector.LatestB
 		return w.klaytn.client.GetLatestBlock(), nil
 	default:
 		return nil, fmt.Errorf("failed to get latest block number: %s is not supported", blockchainType)
+	}
+}
+
+func (w *watchService) IsWatching(blockchainType string) (bool, error) {
+	switch blockchainType {
+	case "ethereum":
+		return w.ethereum.isWatching, nil
+	case "klaytn":
+		return w.klaytn.isWatching, nil
+	default:
+		return false, fmt.Errorf("failed to get watch status: %s is not supported", blockchainType)
 	}
 }
